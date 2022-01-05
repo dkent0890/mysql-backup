1b34d74855a7d6975edc16e92c83e97cdaa957f5
dkent0890 <dkent0890@gmail.com>
2022-01-05 12:27:04 +0000

    commit_backup_databases

--- backup_databases.sh
+++ backup_databases.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-USER="dima_user"
-PASSWORD="Lbvf@0890"
+USER="username"
+PASSWORD="password"
 OUTPUT="/usr/home/backup" #директория для хранения резервных копий
  
 databases=mysql --user=$USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database
  
 for db in $databases; do
-    if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]] ; then
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "performance_schema" ]] ; then
         echo "Dumping database: $db"
         mysqldump --user=$USER --password=$PASSWORD --databases $db > $OUTPUT/date +%Y%m%d.$db.sql
         gzip $OUTPUT/date +%Y%m%d.$db.sql
