17e0b616b9743c3b85fe1a6d33835a1835389c83
dkent0890 <dkent0890@gmail.com>
2022-01-12 13:04:07 +0000

    add backup_databases

--- backup_databases.sh
+++ backup_databases.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-USER="username"         #USER для базы нанных MySQL 
-PASSWORD="my_password"  #PASSWORD для базы нанных MySQL
-OUTPUT="/*/*/backup"    #директория для хранения резервных копий 
+USER="dima_user"         #USER для базы нанных MySQL 
+PASSWORD="Lbvf@0890"  #PASSWORD для базы нанных MySQL
+OUTPUT="/home/vagrant/backup"    #директория для хранения резервных копий 
 #/*/*/backup/daily/   директория backup записей за денелю 
 #/*/*/backup/monthly/ директория backup записей за месяц 
 #/*/*/backup/weekly/  директория backup записей за год
-WEEK="Saturday" #день недели дня еженедельного копированияя
-WDATE="**"      #дата для ежемесячного копирования
-DBACKUP="*"     #колическтво backup записей за денелю
-MBACKUP="*"     #количество backup записей за месяц
-WBACKUP="*"     #количество backup записей за год
+WEEK="Wednesday" #день недели дня еженедельного копированияя
+WDATE="12"      #дата для ежемесячного копирования
+DBACKUP="2"     #колическтво backup записей за денелю
+MBACKUP="2"     #количество backup записей за месяц
+WBACKUP="2"     #количество backup записей за год
 
 
-mkdir -p "$OUTPUT/daily/"
-mkdir -p "$OUTPUT/monthly/"
-mkdir -p "$OUTPUT/weekly/"
+mkdir -p "$OUTPUT/daily"
+mkdir -p "$OUTPUT/monthly"
+mkdir -p "$OUTPUT/weekly"
+
+DIRdaily="$OUTPUT/daily/$(date +%Y.%m.%d)"
+DIRmonthly="$OUTPUT/monthly/$(date +%Y.%m.%d)"
+DIRweekly="$OUTPUT/weekly/$(date +%Y.%m.%d)"
 
 my_function () {
-	mysqldump -u$USER -p$PASSWORD --databases "$db" >>  "$1""$(date +%Y%m%d)"."$db".sql
-gzip "$1""$(date +%Y%m%d)"."$db".sql
+mysqldump -u$USER -p$PASSWORD --databases "$db" >>  "$1"/"$db".sql
+gzip "$1"/"$db".sql
 }
 
 databases=$(mysql -u$USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
@@ -28,20 +32,28 @@ for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "performance_schema" ]] ; then
 	    if [[ $(date +%A) == "$WEEK" ]] && [[ $(date +%d) == "$WDATE" ]]; then
                echo "Dumping database DIR: daily, monthly and weekly -$db"
-               my_function "$OUTPUT/daily/"
-               my_function "$OUTPUT/monthly/"
-               my_function "$OUTPUT/weekly/"
-               elif [[ $(date +%A) == "$WEEK" ]] ; then
+               mkdir -p "$DIRdaily"
+               my_function "$DIRdaily"
+               mkdir -p "$DIRmonthly"
+               cp "$DIRdaily"/* "$DIRmonthly"/*
+               mkdir -p "$DIRweekly"
+               cp "$DIRdaily"/* "$DIRweekly"/*
+            elif [[ $(date +%A) == "$WEEK" ]] ; then
                echo "Dumping database DIR: daily and monthly -$db"    
-               my_function "$OUTPUT/daily/"
-               my_function "$OUTPUT/monthly/"
-               elif [[ $(date +%d) == "$WDATE" ]] ; then
+               mkdir -p "$DIRdaily"
+               my_function "$DIRdaily"
+               mkdir -p "$DIRmonthly"
+               cp "$DIRdaily"/* "$DIRmonthly"/*
+            elif [[ $(date +%d) == "$WDATE" ]] ; then
                echo "Dumping database DIR: daily and weekly -$db"
-               my_function "$OUTPUT/daily/"
-               my_function "$OUTPUT/weekly/"
+               mkdir -p "$DIRdaily"
+               my_function "$DIRdaily"
+               mkdir -p "$DIRweekly"
+               cp "$DIRdaily"/* "$DIRweekly"/*
             else
                echo "Dumping database DIR: daily -$db"
-               my_function "$OUTPUT/daily/"
+               mkdir -p "$DIRdaily"
+               my_function "$DIRdaily"
             fi
     fi
 done
